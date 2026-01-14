eb04491251da59cddbebed9026470812fc6ea5fb
Charlie Lambert <151550145+charlielambert1@users.noreply.github.com>
2026-01-14 20:02:26 +0000

    lesson 6

--- app.py
+++ app.py
@@ -1,79 +1,81 @@
-num1 = 25
-num2 = 7
+temperature = 25
 
-print("arithmetic operations")
-sum_result = num1 + num2
-print(f'Sum: {sum_result}')
-difference_result = num1 - num2
-print(f'Difference: {difference_result}')
-product_result = num1 * num2
-print(f'Product: {product_result}')
-division_float_result = num1 / num2
-print(f'Division (float): {division_float_result}')
-division_int_result = num1 // num2
-print(f'Division (int): {division_int_result}')
-modulus_result = num1 % num2
-print(f'Modulus: {modulus_result}')
-exponentiation_result = num1 ** num2
-print(f'Exponentiation: {exponentiation_result}')
+if temperature > 30:
+    print("It's a hot day")
+elif temperature < 20:
+    print("It's a cold day")
+else:
+    print("It's a lovely day")
+
+#-------------------------
+x=10
+y=5
+
+print(x == y)
+print(x != y)
+print(x > y)
+
+#------------------------
+
+age = 16
+print(f'age: {age}')
+
+has_id = True
+
+if age >= 18 and has_id:
+    print("You are allowed to enter")
+else:
+    print("You are not allowed to enter")
+
+is_student = True
+is_senior = False
+
+if is_student or is_senior:
+    print("You get a discount")
 
 
-BIDMAS_result = num1 + num2 * 2
-print(f'BIDMAS Result (num1 + num2 * 2): {BIDMAS_result}')
-BIDMAS_result_parentheses = (num1 + num2) * 2
-print(f'BIDMAS Result with Parentheses ((num1 + num2) * 2): {BIDMAS_result_parentheses}')
+#------------------------
 
-#--- Comparison Operations ---
-print("\ncomparison operations")
+is_logged_in = False
 
-age = 20
+if not is_logged_in:
+    print("Please log in to continue")
 
-is_exaactly_20 = (age == 20)
-print(f'Is age exactly 20? {is_exaactly_20}')
+#------------------------
 
-is_age_not_20 = (age != 20)
-print(f'Is age not 20? {is_age_not_20}')    
+#check positive negative or zero
 
-is_age_greater_than_18 = (age > 18)
-print(f'Is age greater than 18? {is_age_greater_than_18}')
+number = int(input("Enter a number: "))
 
-is_age_less_than_18 = (age < 18)
-print(f'Is age less than 18? {is_age_less_than_18}')
+if number > 0:
+    print("The number is positive")
+elif number < 0:
+    print("The number is negative")
+else:
+    print("The number is zero")
 
-is_age_greater_equal_18 = (age >= 18)  
-print(f'Is age greater than or equal to 18? {is_age_greater_equal_18}')
+#------------------------
+#simple grading system
 
-is_age_less_equal_18 = (age <= 18)
-print(f'Is age less than or equal to 18? {is_age_less_equal_18}')
+score_str = input("Enter your score (0-100): ")
+score = int(score_str)
+if score >= 90:
+    print("Grade: A")
+elif score >= 80:
+    print("Grade: B")
+elif score >= 70:
+    print("Grade: C")
+else:
+    print("Grade: F")   
 
-print("--------------------------------")
+#------------------------
+# #is the year a leap year
+# 
+year_str = input("Enter a year: ")
+year = int(year_str)
 
-print("is 10 = 10 ?", 10 == 10)
-
-print("is apple = orange ?", "apple" == "orange")
-
-print(f'is apple = Apple ?',"apple" == "Apple")
-
-#-------------
-#BMI CALCULATOR
-print("\nBMI CALCULATOR")
-height = float(input("Enter your height in meters: "))
-weight = float(input("Enter your weight in kilograms: "))
-bmi = weight / (height ** 2)
-bmi_rounded = round(bmi, 2)
-print(f'Your BMI is: {bmi_rounded}')
-
-healthy_bmi = 18.5 <= bmi <= 24.9
-unhealthy_bmi = bmi < 18.5 or bmi > 24.9
-
-print(f'Is your BMI in the healthy range (18.5 - 24.9)? {healthy_bmi}')
-
-#-------------------------------
-
-rectangle_length = float(input("\nEnter the length of the rectangle: "))
-rectangle_width = float(input("Enter the width of the rectangle: "))
-area = rectangle_length * rectangle_width
-print(f'The area of the rectangle is: {area}')
-
-is_area_greater_than_100 = area > 100
-print(f'Is the area greater than 100? {is_area_greater_than_100}')
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(f"{year} is a leap year")
+else:
+    print(f"{year} is not a leap year")
+#------------------------ 
