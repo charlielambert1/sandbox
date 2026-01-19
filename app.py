422b06023c6327e6d2c923013c830d1e7ac61743
Charlie Lambert <151550145+charlielambert1@users.noreply.github.com>
2026-01-19 20:41:03 +0000

    lesson 8 - part 1

--- app.py
+++ app.py
@@ -1,130 +1,37 @@
-#For Loop
+#creating strings 
 
-names = ["Alice", "Bob", "Charlie"]
+single_q = 'Hello, World!'
+double_q = "Hello, World!"
+quote_in_string = "He said, 'Hello!'"
+multi_line = """This is a
+multi-line string."""
 
-for name in names:
-    print(name) 
-
-
-#------------------
-
-for i in range(1,6):
-    print(i)
-
-
-#------------------
-
-count = 0 
-
-while count < 5:
-    print("Count is:", count)
-    count = count + 1
-
-#------------------
-
-numbers = [10, 20, 30, 40, 50]
-
-x = 30
-
-for num in numbers:
-    print('checking', num)
-    if num == x:
-        print('Found', x)
-        break
-    print('Not found yet')
+#printing strings
+print(single_q)
+print(double_q)
+print(quote_in_string)
+print(multi_line)
 
 
-#------------------
+my_word = "Python"
 
-number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+first_char = my_word[0]
+last_char = my_word[-1]
+print("First character:", first_char)
+print("Last character:", last_char)
 
-for num in number_list:
-    if num == 5:
-        continue
-    print(num)
+substring = my_word[1:4]
+print("Substring (1 to 4):", substring)
 
-#------------------
+substring_from_start = my_word[:3]
+print("Substring (start to 3):", substring_from_start)
 
-numbers_list = [10, 20, 30, 40, 50]
+substring_to_end = my_word[2:]
+print("Substring (2 to end):", substring_to_end)
 
-for num in numbers_list:
-    print(num)
+greeting = "Hello"
 
-#------------------
+new_greeting = greeting[0] + "ey"
 
-string = "Hello, World!"
+print("Modified greeting:", new_greeting)
 
-for char in string:
-    if char == ',' or char == ' ' :
-        continue
-    print(char)
-
-#------------------
-
-for i in range(1, 11):
-    print(i)
-
-#------------------
-
-my_dict = {'a': 1, 'b': 2, 'c': 3}
-
-for key in my_dict:
-    print(key)
-
-for key, value in my_dict.items():
-    print(key, value)
-
-#------------------
-
-count = 0 
-
-while count <5:
-    print("Count is:", count)
-    count += 1
-
-#------------------
-
-correct_password = "secure123"
-attempt = 0
-max_attempts = 3
-
-# while attempt < max_attempts:
-#     user_input = input("Enter password: ")
-#     if user_input == correct_password:
-#         print("Access granted")
-#         break
-#     if attempt == max_attempts - 1:
-#         print("Access denied")
-#         break
-#     else:
-#         print("Incorrect password")
-#         attempt += 1
-
-
-#------------------
-
-number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-for num in number_list:
-    if num == 7:
-        print("found 7, stopping the loop")
-        break
-    print(num)
-
-
-for num in number_list:
-    if num % 2 == 0:
-        continue
-    print(num)
-
-#------------------
-
-num = [10,-5,2,-1,8,-3]
-sum =0 
-
-for n in num:
-    if n % 2 ==0:
-        sum += n
-        print("sum so far:", sum)
-    else:
-        continue
