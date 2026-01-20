3877cb810ec365987f8faeb14827f0ed1c432a4b
Charlie Lambert <151550145+charlielambert1@users.noreply.github.com>
2026-01-20 20:59:19 +0000

    Adding, removing from lists

--- app.py
+++ app.py
@@ -1,60 +1,53 @@
-#Lists
+#mutable lists
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
 
-my_mixed_list = [42, "hello", 3.14, True, "world", 7]
+print_first_item = fruits[0]
+print("First fruit:", print_first_item)
+print_last_item = fruits[-1]
+print("Last fruit:", print_last_item)
 
-my_list = ["apple", "banana", "cherry", "date"]
+#####################
+fruits[1] = "blueberry"
+print("Updated fruits:", fruits)
+#####################
+fruits.append("date")
+print("Fruits after appending date:", fruits)
 
-empty_list = []
+fruits.append("blueberry")
+print("Fruits after appending blueberry again:", fruits)
+#####################
+fruits = ["apple", "blueberry", "cherry", "date"]
+fruits.insert(1, "banana")
+print("Fruits after inserting banana at index 1:", fruits)
 
-numbers = [1, 2, 3, 4, 5]
+fruits.insert(0,"kiwi")
+print("Fruits after inserting kiwi at index 0:", fruits)   
+#####################
 
-list_from_string = "hello"
+fruits.remove("banana")
+print("Fruits after removing banana:", fruits)
 
-list_from_string = list(list_from_string) 
+if "grape" in fruits:
+    fruits.remove("grape")
 
-######
+#####################
+print("Fruits before popping the last item:", fruits)
+item_removed = fruits.pop(0)
+print("Fruits after popping the last item:", fruits)
+print("Item removed:", item_removed)
+#####################
 
-first_fruit = my_list[0]  # "apple"
-second_fruit = my_list[1]  # "banana"
-last_fruit = my_list[-1]  # "date"
-second_last_fruit = my_list[-2]  # "cherry"
+fruits = ["apple", "banana", "cherry", "date"]
+del fruits[2]
+print("Fruits after deleting item at index 2:", fruits)
+#####################
 
-print(first_fruit, second_fruit, last_fruit, second_last_fruit)
+print("Fruits before deleting a slice:", fruits)
+del fruits[1:2]
+print("Fruits after deleting a slice:", fruits)
 
-##########################################################
-fruits_slice = my_list[1:4:2]  # ["banana", "cherry", "date"]
-print(fruits_slice)
+#####################
 
-fruits_to_end = my_list[2:]  # ["cherry", "date"]
-print(fruits_to_end)
-
-fruits_from_start = my_list[:3]  # ["apple", "banana", "cherry"]
-print(fruits_from_start)
-##########################################################
-list_length = len(my_list)  # 4
-print(list_length)
-
-
-##########################################################
-my_list.append("elderberry")
-print(my_list)  # ["apple", "banana", "cherry", "date", "elderberry"]
-my_list.insert(1, "blueberry")
-print(my_list)  # ["apple", "blueberry", "banana", "cherry", "date", "elderberry"]
-my_list.remove("cherry")
-
-##########################################################
-list_1 = [1, 2, 3]
-list_2 = [4, 5, 6]
-
-combined_list = list_1  +  list_2  # [1, 2, 3, 4, 5, 6]
-print(combined_list)
-
-printed_list = list_1 * 3  # [1, 2, 3, 1, 2, 3, 1, 2, 3]
-#print(printed_list)
-
-is_2_in_list = 2 in list_1  # True
-is_5_not_in_list = 5 not in list_1  # True
-print(is_2_in_list, is_5_not_in_list)
-
-###########################################################
-
+fruits.clear()
+print("Fruits after clearing all items:", fruits)
